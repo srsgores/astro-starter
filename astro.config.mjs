2d49fbcd8e916f6e4273ceb87093a5521dcf730d
Sean Goresht <seangores@gmail.com>
2023-10-19 16:21:05 -0600

    feat: markdown table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import {defineConfig} from "astro/config";
 import {SITE} from "./src/config";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
 	site: SITE.url,
-	adapter: netlify()
+	adapter: netlify(),
+	markdown: {
+		remarkPlugins: [
+			remarkToc, [remarkCollapse, {test: "Table of contents", summary: "Table of Contents"}]
+		]
+	}
 });
