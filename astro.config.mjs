867190bad970bb1fed769ab281c9b1b9ba9ec3ca
Sean Goresht <seangores@gmail.com>
2023-10-22 22:51:24 -0600

    fix(build): enable netlify SSR edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import rehypeExternalLinks from "rehype-external-links";
 // https://astro.build/config
 export default defineConfig({
 	site: SITE.url,
+	output: "server",
 	adapter: netlify(),
 	markdown: {
 		rehypePlugins: [
